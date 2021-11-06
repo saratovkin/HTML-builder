6a2666325197b67207a65de11372054433601887
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-11-06 17:15:52 +0300

    feat: add fourth task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+
+const copyDir = function () {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), function (err, files) {
+    if (err) {
+      return console.log(err);
+    }
+    files.forEach(item => {
+      fs.copyFile(path.resolve(__dirname, 'files', item), path.resolve(__dirname, 'files-copy', item), function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+  });
+};
+
+copyDir();
